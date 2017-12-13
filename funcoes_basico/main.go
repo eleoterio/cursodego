792a94064e6592f9a93b735da98d42de1a32b22f
felipe eleoterio <eleoterio.eduzz@gmail.com>
2017-12-13 15:50:40 -0200

    Aula 10

--- funcoes_basico/main.go
+++ funcoes_basico/main.go
@@ -11,4 +11,8 @@ func main() {
 	fmt.Printf("Resultado é %d\r\n", resultado)
 	resultado = matematica.Soma(2, 2)
 	fmt.Printf("Resultado da soma %d\r\n", resultado)
+	resultado = matematica.Calculo(matematica.Divisor, 12, 3)
+	fmt.Printf("Divisao: %d\r\n", resultado)
+	resultado, resto := matematica.DivisaoResto(12, 5)
+	fmt.Printf("Divisao é - %d e o resto é - %d \r\n", resultado, resto)
 }
--- funcoes_basico/matematica/generico.go
+++ funcoes_basico/matematica/generico.go
@@ -7,9 +7,21 @@ func Calculo(funcao func(int, int) int, numeroA int, numeroB int) int {
 	return funcao(numeroA, numeroB)
 }
 
-/*
-Multiplica X * Y
-*/
+//Multiplica X * Y
 func Multiplicador(x int, y int) int {
 	return x * y
+}
+
+//Divisor divisão entre numeroA / numeroV
+func Divisor(numeroA int, numeroB int) (resultado int) {
+	resultado = numeroA / numeroB
+	return
+}
+
+//DivisaoResto divide os valores
+//retorno resto da divisão
+func DivisaoResto(x int, y int) (resultado int, resto int) {
+	resultado = x / y
+	resto = x % y
+	return
 }
